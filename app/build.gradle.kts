f8edf94697a3fd1a6a7bc7f12b20f0e32c681634
Zagura <me@zagura.one>
2024-02-05 09:47:12 +0100

    Bumped up version and added changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "one.zagura.IonLauncher"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "y24-v3"
+        versionCode = 5
+        versionName = "y24-v4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
